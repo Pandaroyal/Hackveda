eb5634b42c9eef6916bd1f6fe761fc93dc0888ce
Altamash <hk337121@gmail.com>
2023-03-31 11:01:35 +0530

    new changes

--- Task1.java
+++ Task1.java
@@ -1,41 +1,44 @@
 import java.util.Scanner;
-class Task1{
-    public static String encrypt(String s,int key){
+
+class Task1 {
+    public static String encrypt(String s, int key) {
         String done = "";
-        for(int i=0;i<s.length();i++){
-            int c =(s.charAt(i)+key%26);
-            if(c>122){
-                    done += (char)(c%122+96);
-            }else if(c<97&&c>90){
-                done += (char)(c%90+64);
-            }else{
-                done+=(char)(c);
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            if (c == ' ') {
+                done += (char) c;
+                continue;
             }
+            c += key % 26;
+            done += c > 122 || (c < 97 && c > 90) ? (char) (c - 26) : (char) (c);
         }
         return done;
     }
-    public static String decrypt(String s,int key){
+
+    public static String decrypt(String s, int key) {
         String done = "";
-        for(int i=0;i<s.length();i++){
-            int c =(s.charAt(i)-key%26);
-            if(c<65||(c>90&&c<97)){
-                    done += (char)(c+26);
-            }else{
-                done+=(char)(c);
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            if (c == ' ') {
+                done += (char) c;
+                continue;
             }
+            c -= key % 26;
+            done += c < 65 || (c > 90 && c < 97) ? (char) (c + 26) : (char) (c);
         }
         return done;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Message: ");
-        String s = sc.nextLine();
+        String msg = sc.nextLine();
         System.out.print("Enter Key: ");
         int key = sc.nextInt();
+        String encrypted_msg = encrypt(msg, key);
+        System.out.println("Encrypted Message = " + encrypted_msg);
+        String decrypted_msg = decrypt(encrypted_msg, key);
+        System.out.println("Decrypted Message = " + decrypted_msg);
         sc.close();
-        String encrypt = encrypt(s,key);
-        System.out.println("Encrypted Message = "+encrypt);
-        String decrypt = decrypt(encrypt,key);
-        System.out.println("Decrypted Message = "+decrypt);
     }
 }
